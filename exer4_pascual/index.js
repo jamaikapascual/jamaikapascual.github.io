c35e253af89d59cdfae062c7dda7314811b70ae1
jamaikapascual <jtascual2@up.edu.ph>
2024-03-08 21:45:20 +0800

    Added Function for generateUniqueID

--- exer4_pascual/index.js
+++ exer4_pascual/index.js
@@ -1,14 +1,15 @@
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 import validator from 'validator';
 import { appendFileSync } from 'node:fs';
 
 
 //function for generating unique id
 function generateUniqueID(firstName, lastName){
-    let uniqueID = firstName[0].tolowerCase()+ lastName.tolowerCase();
-    let uniqueString= uuidv4(8);
-    return uniqueID + uniqueString.randomAlphanumeric(8);
+    let uniqueID = firstName[0].toLowerCase()+ lastName.toLowerCase();
+    let uniqueString= uuidv4().slice(0, 8);
+    return uniqueID + uniqueString
 
 }
+
 
 export default generateUniqueID
--- exer4_pascual/text.js
+++
@@ -1,3 +0,0 @@
-import {generateUniqueID} from './index.js'
-console.log("Sample unique IDs:")
-console.log(generateUniqueID("Pascual", "Jam"))
