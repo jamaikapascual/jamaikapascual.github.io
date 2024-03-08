2d8bd84a9b6c60e48169210d2a60158e1a36f8d9
jamaikapascual <jtascual2@up.edu.ph>
2024-03-08 23:05:51 +0800

    Added function for addAcount

--- exer4_pascual/index.js
+++ exer4_pascual/index.js
@@ -12,4 +12,31 @@ function generateUniqueID(firstName, lastName){
 }
 
 
-export default generateUniqueID
+function  addAccount(information){
+    //checks if the first name, last name, and email is a non-empty string
+    if(information[0].length == 0 && information[1].length == 0 && information[2].length == 0) {
+		console.log("Error! First name, last name, and email must be valid.");
+		return false;
+    //checks if age is at least 18
+	}else if(information[3] < 18) {
+		console.log("Error! Age must be at least 18.");
+		return false;
+    //checks if the email is valid
+	}else if(validator.isEmail(information[2]) == false) {
+		console.log("Error! Invalid email.");
+		return false;
+    //saves the data to a new file
+	}else {
+	 	let uniqueID = generateUniqueID(information[0], information[1]);
+	 	let data = information[0] + ',' + information[1] + ',' + information[2] + ',' + information[3] + ',' + uniqueID + '\n';
+	 	try	{
+			appendFileSync("users.txt", data);
+			console.log("Account added!");
+	 		return true;
+		} catch (err) {
+			console.log("Account not added.")
+		}
+	}
+}
+
+export {generateUniqueID, addAccount}
--- exer4_pascual/test.js
+++ exer4_pascual/test.js
@@ -1,3 +1,13 @@
-import generateUniqueID from './index.js'
+import {generateUniqueID, addAccount} from './index.js'
+
+//test cases for generatingUniqueID
 console.log("Sample unique IDs:")
-console.log(generateUniqueID("Meowmeow", "Toby"))
+console.log(generateUniqueID("Alan", "Turing"))
+console.log(generateUniqueID("Jamaika", "Pascual"))
+
+console.log("")
+
+//test cases addAccount
+console.log(addAccount(["Tim", "Berners-Lee", "tim@w3c.com", 25]))
+console.log(addAccount(["Ted", "Nelson", "ted.n@w3c.com", 43]))
+
